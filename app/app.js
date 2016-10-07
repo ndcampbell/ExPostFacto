f3ffbc0c31cf7281ef8b185d4be697120af74aea
Doug Campbell <dcampbell@gogoair.com>
2016-10-07 09:50:49 -0500

    added app.js and started backend app

---
+++ app/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+
+app.use
+app.get('/', function (req, res) {
+    var data = [
+        {
+            title: "test card 1",
+            description: "this is test one",
+        },
+        {
+            title: "test card 2",
+            description: "this is test two",
+        }
+    ];
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(data));
+});
+
+
+app.listen(3001, function() {
+    console.log('Example app listening on port 3001');
+});
