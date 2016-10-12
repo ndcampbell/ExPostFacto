ffcd3c3092cd0f7d6b141b0bbf781d1b8ae2832b
Doug Campbell <dcampbell@gogoair.com>
2016-10-11 19:33:04 -0500

    removed bootstrap grid

--- frontend/src/mainGrid.js
+++ frontend/src/mainGrid.js
@@ -1,41 +1,43 @@
 import React from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
 import CreateCardButton from './createCardButton.js';
 import Cards from './card.js';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-const styles = {
-    leftSide: {
-      height: '100%'
-    },
-    rightSide: {
-      height: '100%'
-    }
-  };
-
 var MainGrid = React.createClass({
-    render: function() {
-        return (
-        <MuiThemeProvider>
-            <Grid>
-                <Row className="show-grid">
-                    <Col xs={6} md={6} style={ styles.leftSide }>
-                        <center><h2>What went well?</h2></center>
-                        <br />
-                        <Cards />
-                        <CreateCardButton cardcol={1}/>
-                    </Col>
-                    <Col xs={6} md={6} style={ styles.rightSide }>
-                        <center><h2>What didn't go well?</h2></center>
-                        <br />
-                        <Cards />
-                        <CreateCardButton cardcol={2}/>
-                    </Col>
-                </Row>
-            </Grid>
-          </MuiThemeProvider>
-        );
+  render: function() {
+    const styles = {
+      divWrap: {
+        width: '1000px',
+        margin: '0 auto',
+      },
+      divLeft: {
+        float: 'left',
+        width: '480px',
+      },
+      divRight: {
+        float: 'right',
+        width: '480px',
+      }
     }
+    return (
+      <MuiThemeProvider>
+        <div style={styles.divWrap}>
+          <div style={styles.divLeft}>
+            <center><h2>What went well?</h2></center>
+              <br />
+              <Cards />
+              <CreateCardButton cardcol={1}/>
+          </div>
+          <div style={styles.divRight}>
+            <center><h2>What didn't go well?</h2></center>
+              <br />
+              <Cards />
+              <CreateCardButton cardcol={1}/>
+          </div>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 });
 
 module.exports = MainGrid;
